82c6049e61b4ad237896ea1bae9d762acd29c0e3
Adrian Lara <adrianblara@gmail.com>
2018-01-02 10:54:06 -0700

    set up vcr

--- app/controllers/stores/search_controller.rb
+++ app/controllers/stores/search_controller.rb
@@ -1,7 +1,7 @@
 class Stores::SearchController < ApplicationController
 
   def index
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType&pageSize=10&apiKey=#{ENV["BESTBUY_KEY"]}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params[:q]},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=10&apiKey=#{ENV["BESTBUY_KEY"]}")
     raw_stores = JSON.parse(response.body, symbolize_names: true)
     binding.pry
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,11 +6,16 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+require 'vcr'
 require 'database_cleaner'
 
 DatabaseCleaner.strategy = :truncation
 # Add additional requires below this line. Rails is not loaded until this point!
 
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
