e047cbc30a8d6d1016940e0607311b9a2c164497
Adrian Lara <adrianblara@gmail.com>
2018-01-02 10:50:26 -0700

    add figaro. consuming store search api is WIP

--- app/controllers/stores/search_controller.rb
+++ app/controllers/stores/search_controller.rb
@@ -1,7 +1,9 @@
 class Stores::SearchController < ApplicationController
 
   def index
-
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType&pageSize=10&apiKey=#{ENV["BESTBUY_KEY"]}")
+    raw_stores = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
   end
-  
+
 end
