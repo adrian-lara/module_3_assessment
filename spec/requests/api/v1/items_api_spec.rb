88cb4218ddbad6cd75e71b2f082dd139cece0eec
Adrian Lara <adrianblara@gmail.com>
2018-01-02 09:27:02 -0700

    single item api endpoint written and tested

--- spec/requests/api/v1/items_api_spec.rb
+++ spec/requests/api/v1/items_api_spec.rb
@@ -24,8 +24,22 @@ describe "Items API" do
       expect(item).not_to have_key("updated_at")
     end
   end
+
+  it "can return a single item" do
+    get "/api/v1/items/#{Item.first.id}"
+
+    expect(response).to be_success
+
+    item = JSON.parse(response.body)
+
+    expect(item).to have_key("id")
+    expect(item).to have_key("name")
+    expect(item).to have_key("description")
+    expect(item).to have_key("image_url")
+    expect(item).not_to have_key("created_at")
+    expect(item).not_to have_key("updated_at")
+  end
 end
 
-# When I send a GET request to `/api/v1/items`
-# I receive a 200 JSON response containing all items
-# And each item has an id, name, description, and image_url but not the created_at or updated_at
+# When I send a GET request to `/api/v1/items/1`
+# I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
