6eb3be48ae7c17464d65a0ec905460c5b49afd3c
Adrian Lara <adrianblara@gmail.com>
2018-01-02 11:28:35 -0700

    store search feature refactored. and vcr cassette refreshed as final check

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -4,12 +4,16 @@ class StoreService
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=10&apiKey=#{ENV["BESTBUY_KEY"]}")
     parsed_response = JSON.parse(response.body, symbolize_names: true)
 
-    raw_stores = parsed_response[:stores]
-    stores = raw_stores.map { |raw_store| Store.new(raw_store) }
-
-    stores_count = parsed_response[:total]
-
-    StoreSearchResult.new(stores_count, stores)
+    create_store_search_result(parsed_response)
   end
+
+  private
+
+    def self.create_store_search_result(parsed_response)
+      stores_count = parsed_response[:total]
+      stores = parsed_response[:stores].map { |raw_store| Store.new(raw_store) }
+
+      StoreSearchResult.new(stores_count, stores)
+    end
 
 end
