f5c458c3df4af44b0e34309f4b2ba58e2c4a7549
Adrian Lara <adrianblara@gmail.com>
2018-01-02 09:42:42 -0700

    create item api endpoint written and tested

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -13,4 +13,14 @@ class Api::V1::ItemsController < ApplicationController
     item.destroy
   end
 
+  def create
+    render json: Item.create(item_params), status: 201
+  end
+
+  private
+
+    def item_params
+      params.permit(:name, :description, :image_url)
+    end
+
 end
--- spec/requests/api/v1/items_api_spec.rb
+++ spec/requests/api/v1/items_api_spec.rb
@@ -47,7 +47,26 @@ describe "Items API" do
 
     expect(Item.count).to eq(2)
   end
-end
 
-# When I send a DELETE request to `/api/v1/items/1`
-# I receive a 204 JSON response if the record is successfully deleted
+  it "can create an item" do
+    params = {
+      name: "Best Item",
+      description: "This is the best item ever",
+      image_url: "http://via.placeholder.com/300"
+    }
+
+    post "/api/v1/items"
+
+    expect(response.code).to eq("201")
+    expect(Item.count).to eq(4)
+
+    item = JSON.parse(response.body)
+
+    expect(item).to have_key("id")
+    expect(item).to have_key("name")
+    expect(item).to have_key("description")
+    expect(item).to have_key("image_url")
+    expect(item).not_to have_key("created_at")
+    expect(item).not_to have_key("updated_at")
+  end
+end
